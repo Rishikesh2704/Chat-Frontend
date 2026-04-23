911895324a3fe3591b3c82709a5933940b1da56a
Rishikesh <DarnuleRishikesh@gmail.com>
2026-04-23 19:44:31 +0530

    added account route

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,18 @@
-import './Home.css'
+import { Link } from "react-router";
+import "./Home.css";
 
-export default function Home(){
-    return(
-        <div className="Home_Wrapper">
-            <nav className="Navbar">
-                <h1 id="AppName">Convo</h1>
-            </nav>
-            <section className="Chat_Space">
-            </section>
+export default function Home() {
+  return (
+    <div className="Home_Wrapper">
+      <nav className="Navbar">
+        <div className="Header">
+          <h1 id="AppName">Convo</h1>
+          <a href="/account" className="Account">
+            <i className="fa-solid fa-circle-user"></i>
+          </a>
         </div>
-    )
+      </nav>
+      <section className="Chat_Space"></section>
+    </div>
+  );
 }
