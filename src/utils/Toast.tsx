2ddbcff3f297ab29f5f83432ef2bfe970ed421d3
Rishikesh <DarnuleRishikesh@gmail.com>
2026-05-04 18:23:29 +0530

    minor change

--- src/utils/Toast.tsx
+++ src/utils/Toast.tsx
@@ -1,15 +1,26 @@
-import './toast.css'
-
+import "./toast.css";
+import { useEffect, useState } from "react";
 type toastProps = {
-    type?:string;
-    message?:string;
-}
-export default function Toast(props:toastProps){
-    const { type="Alert", message="No Message"} = props;
-    return(
+  type?: string;
+  message?: string;
+  time: number;
+};
+export default function Toast(props: toastProps) {
+  const { type = "Alert", message = "No Message", time = 5000 } = props;
+  const [showToast, setShowToast] = useState<boolean>(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowToast(false);
+    }, time);
+  }, []);
+  return (
+    <>
+      {showToast&&
         <div className="toastWrapper">
-            <h1 className="toastType">{type}</h1>
-            <p className="toastMessage">{message}</p>
+          <h1 className="toastType">{type}</h1>
+          <p className="toastMessage">{message}</p>
         </div>
-    )
+      }
+    </>
+  );
 }
