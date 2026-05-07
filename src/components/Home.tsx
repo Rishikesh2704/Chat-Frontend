424401490a598cf57e1e58f12f4aeed2a4e09421
Rishikesh <DarnuleRishikesh@gmail.com>
2026-05-07 15:05:59 +0530

    added chat body

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,17 @@ import "./Home.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+type User = {
+  id: number;
+  username: string;
+  profile?: string;
+};
+
 export default function Home() {
   const navigate = useNavigate();
-  const [ users, setUsers ] = useState([])
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedChat, setSelectChat] = useState<User | undefined>();
+  const [message, setMessage] = useState<string>('')
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -15,7 +23,7 @@ export default function Home() {
             withCredentials: true,
           },
         );
-        setUsers(data?.data)
+        setUsers(data?.data);
       } catch (error: any) {
         if (error.response.data.message === "Unauthorized")
           navigate("/authentication/login");
@@ -43,25 +51,50 @@ export default function Home() {
         </form>
 
         <div className="Chat_Friends">
-        {users&& users.map((user:any) => (
-          <>
-          
-           {console.log(user)}
-          <div className="User_Wrapper">
-            <figure>
-              <i className="fa-solid fa-circle-user"></i>
-            </figure>
-            <div className="User_Details">
-              <h2>{user.username}</h2>
-              <p>Hi</p>
-            </div>
-          </div>
-          </>
-        ))}
-          
+          {users &&
+            users.map((user: any) => (
+              <>
+                <div className="User_Wrapper" onClick={() => setSelectChat(user)}>
+                  <figure>
+                    <i className="fa-solid fa-circle-user"></i>
+                  </figure>
+                  <div className="User_Details">
+                    <h2>{user.username}</h2>
+                    <p>Hi</p>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </nav>
-      <section className="Chat_Space"></section>
+      <section className="Chat_Space">
+        {selectedChat && (
+          <>
+            <div className="chatheader">
+              <div className="profile">
+                <i className="fa-solid fa-circle-user"></i>
+                <h1>{selectedChat.username}</h1>
+              </div>
+              <div className="chatheader_options">
+                <i className="fa-solid fa-call"></i>
+              </div>
+            </div>
+
+            <div className="chat_main">
+              <div className="chat_messages">
+
+              </div>
+              <form className="message_form">
+                <label htmlFor="message_input" >message</label>
+                <input type='text' id='message_input' onChange={(e) => setMessage(e.target.value) } value={message}></input>
+                <button type="submit" aria-label="send message">
+                  <i className="fa-regular fa-paper-plane"></i>
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </section>
     </div>
   );
 }
